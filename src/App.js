bfc3b569c73bf2b138b853722d56662e66aa266a
Maxime Hallary <hallary.maxime@gmail.com>
2021-09-13 17:54:23 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import mergeImages from "merge-images";
+import background from "./images/iOS-13-wallpaper.jpg";
+import { useState } from "react";
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>QR wallpaper</h1>
+        {/* <img className="App-logo" alt="logo" /> */}
+        <div>
+          {selectedImage && (
+            <div>
+              <img
+                alt="not fount"
+                width={"250px"}
+                src={URL.createObjectURL(selectedImage)}
+              />
+              <br />
+              <button onClick={() => setSelectedImage(null)}>Retirer</button>
+              <button
+                onClick={() => {
+                  mergeImages([
+                    background,
+                    {
+                      src: URL.createObjectURL(selectedImage),
+                      x: 170,
+                      y: 1700,
+                    },
+                  ]).then((b64) => (document.querySelector("img").src = b64));
+                }}
+              >
+                Générer
+              </button>
+            </div>
+          )}
+          <br />
+          <input
+            type="file"
+            name="myImage"
+            onChange={(event) => {
+              console.log(event.target.files[0]);
+              setSelectedImage(event.target.files[0]);
+            }}
+          />
+        </div>
       </header>
     </div>
   );

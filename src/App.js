25ea79ab514d78bbfb6f225a03064f9f75816185
Maxime Hallary <hallary.maxime@gmail.com>
2021-09-14 11:42:45 +0200

    add Background select

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import background from "./images/iOS-13-wallpaper.jpg";
 import { useState } from "react";
 
 function App() {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedBackground, setSelectedBackground] = useState(null);
+  const [selectedQr, setSelectedQr] = useState(null);
 
   return (
     <div className="App">
@@ -12,22 +13,22 @@ function App() {
         <h1>QR wallpaper</h1>
         {/* <img className="App-logo" alt="logo" /> */}
         <div>
-          {selectedImage && (
+          {selectedQr && (
             <div>
               <img
                 alt="not fount"
                 width={"250px"}
-                src={URL.createObjectURL(selectedImage)}
+                src={URL.createObjectURL(selectedQr)}
               />
               <br />
-              <button onClick={() => setSelectedImage(null)}>Retirer</button>
+              <button onClick={() => selectedQr(null)}>Retirer</button>
               <button
                 onClick={() => {
                   mergeImages([
-                    background,
+                    background || URL.createObjectURL(selectedBackground),
                     {
-                      src: URL.createObjectURL(selectedImage),
-                      x: 170,
+                      src: URL.createObjectURL(selectedQr),
+                      x: 220,
                       y: 1700,
                     },
                   ]).then((b64) => (document.querySelector("img").src = b64));
@@ -38,12 +39,25 @@ function App() {
             </div>
           )}
           <br />
+          Ajouter un fond d'écran
           <input
             type="file"
-            name="myImage"
+            name="myBackground"
+            placeholder="Ajouter un fond d'écran"
             onChange={(event) => {
               console.log(event.target.files[0]);
-              setSelectedImage(event.target.files[0]);
+              setSelectedBackground(event.target.files[0]);
+            }}
+          />
+          <br />
+          Ajouter un QR code
+          <input
+            type="file"
+            name="myQr"
+            placeholder="Ajouter un QR code"
+            onChange={(event) => {
+              console.log(event.target.files[0]);
+              setSelectedQr(event.target.files[0]);
             }}
           />
         </div>
